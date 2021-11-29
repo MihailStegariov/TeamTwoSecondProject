cdf52d72875f6b480077e915acb5ae39aa85a6ae
Mihail <Mihail@LAPTOP-DTKS6POR.fios-router.home>
2021-11-28 23:07:00 -0500

    Name Added

--- src/team/two/project/Homework.java
+++ src/team/two/project/Homework.java
@@ -7,9 +7,8 @@ public class Homework {
 		System.out.println(" Lets try to pull and push");
 		
 		System.out.println("Pushing a new method...");
-	}
-	
-	public static void addMethod (int x, int y) {
-		System.out.println(x + y);
+		
+		String teamName = "TeamTwo";
+		System.out.println(teamName);
 	}
 }
